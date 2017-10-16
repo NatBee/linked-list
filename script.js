208fa15ec76c03bc22e0127baffa27b49b1fefa2
Sabrina Kennedy <skenne21@gmail.com>
2017-10-15 20:54:01 -0600

    Add remove function to js

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 var userInputWebsite = document.querySelector("#website-title");
 var userInputUrl = document.querySelector("#website-url");
 var enterButton = document.querySelector(".enter-button");
+var deleteButton = document.querySelector("#delete-button");
 var cardBody = $(".card-body");
 
-
-enterButton.addEventListener('click', addCard); 
+userInputUrl.addEventListener("keyup", enterButtonEnabled);
+userInputWebsite.addEventListener("keyup", enterButtonEnabled);
+enterButton.addEventListener("click", addCard);
  
- function addCard() {
+
+
+function enterButtonEnabled() {
+  if (enterButton.disabled =true) {
+    enterButton.removeAttribute("disabled", false);
+  }
+}
+
+ 
+function addCard() {
+  // while
   userInputWebsite = userInputWebsite.value;
   userInputUrl = userInputUrl.value;
-  cardBody.prepend("<h1> userInputWebsite </h1>" + "<a> userInputUrl </a>");
-  console.log ("add card function" + userInputWebsite + userInputUrl);
+  cardBody.prepend('<section class="card-body"><article class="sample-card"><h1>' + userInputWebsite + '</h1><a>' + userInputUrl + '</a><button id="read-button">Read</button>' + '<button id="delete-button>Delete</button></article></section>');
+  console.log ("add card function", userInputWebsite + userInputUrl);
 }
-    
+
+function removeCard (){
+  deleteButton.addEventListener("click", function(){
+    addcard.remove
+  });
+}
+   
+
+
 
 
 
