bbe2efd8675e3180f15256c5539883d9aaabe541
Sabrina Kennedy <skenne21@gmail.com>
2017-10-17 13:02:30 -0600

    Add read toggle

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 var userInputWebsite = document.querySelector("#website-title");
 var userInputUrl = document.querySelector("#website-url");
 var enterButton = document.querySelector(".enter-button");
-var deleteButton = document.querySelector("#delete-button");
 var cardBody = $(".card-body");
+  var deleteButton = document.querySelector("#delete-button");
+
 
 userInputUrl.addEventListener("keyup", enterButtonEnabled);
 userInputWebsite.addEventListener("keyup", enterButtonEnabled);
 enterButton.addEventListener("click", addCard);
+$('.card-body').on('click', '.read-button', toggleRead);
+$('.card-body').on('click', '.deleteButton', removeCard);  
  
 
 
-function enterButtonEnabled() {
-  if (enterButton.disabled =true) {
-    enterButton.removeAttribute("disabled", false);
-  }
-}
+// function enterButtonEnabled() {
+//   if (enterButton.disabled =true) {
+//     enterButton.removeAttribute("disabled", false);
+//   }
+// }
 
 function addCard() {
   var id = $.now();
@@ -22,50 +25,31 @@ function addCard() {
     $(`<article id="${id}" class="sample-card">
       <h1>${userInputWebsite.value}</h1>
       <a class="website-link">${userInputUrl.value}</a>
-      <button id="${id}" class="read-button">Read</button>
+      <button id="${id}" class="read-button ">Read</button>
       <button id="delete-button" onClick="removeCard(${id})">Delete</button>
     </article>`).appendTo(".card-body");
+
     console.log ("add card function", userInputWebsite + userInputUrl);
 }
-
-deleteButton.addEventListener("click", removeCard); 
 
 function removeCard(id) {
     $('#'+id).remove()
     }
 
-function addRemoveRead(id) {
-  $('#'+id).hover()
-$('read-button').hover(
-       function(){ $(this).addClass('hover') },
-       function(){ $(this).removeClass('hover') },
-)}
+function toggleRead() { 
+$(this).closest('article').toggleClass('sample-card-read') 
+console.log ('.sample-card')
 
-// $("read-button").click(function() {
-//   $(this).toggleClass(".clicked")
-// })
+}
 
-// readButton.addEventListener("click", addReadClass);
-
-// function addReadClass(id) {
-//   $('#'+id);
-// $("read-button").click(function() {
-//   $(this).toggleClass(".clicked:focus");
-// });
-// }
-
-// $('#'+id).on('click', function () {
-//   $(this).toggleClass('clicked');
-// });
+function cardInputReset() {
+  $('userInputUrl').val('');
+  $('userInputWebsite').val('');
+  $('userInputWebsite').focus();
+  enterButton.disabled = true;
+}
 
 
-// function addReadClass(id) {
-//   $('#'+id).prop
-// }
-
-// function toggleColor() {
-//   $("read-button").classlist(#F05A28);
-// }
    
 
 
